30e1388d6e4e8d98398c1b492694a74eeb82f683
Leonardo Rangel De Sousa <lrsousa2@stefanini.com>
2016-03-28 14:41:26 -0300

    criado metodos para ler diretorio e filtar apenas os arquivos DAT

--- src/com/leitor/LeitorArquivo.java
+++ src/com/leitor/LeitorArquivo.java
@@ -1,16 +1,56 @@
 package com.leitor;
 
-import com.eunumeradores.BusinessArea;
-import com.modelos.Customer;
-import com.tinyclasses.Cnpj;
+import java.io.File;
+import java.io.FileFilter;
+import java.io.IOException;
+import java.nio.file.DirectoryIteratorException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class LeitorArquivo {
-	public static void main(String[] args) {
-		Customer customer = new Customer(new Cnpj("111111111"), "leonardo", BusinessArea.RURAL);
+	static final String userhome = System.getProperty("user.home");
+	static final Path in = Paths.get(userhome + "/data/in");
+	static final Path out = Paths.get(userhome + "/data/ou");
+	
+	public static void main(String[] args) throws IOException {
 		
-		System.out.println(customer.getName());
-		System.out.println(customer.getBusinessArea());
-		System.out.println(customer.getCnpj().getCnpj());
+		for (Path path : listaArquivosDat(in)) {
+			System.out.println(path.getFileName());
+		}
+		
+		
+//		File dir = new File(in);
+//		File[] files = dir.listFiles(filter);
+//		
+//		for (File file : files) {
+//			
+//		}
 		
 	}
+	
+	static List<Path> listaArquivosDat(Path diretorio) {
+		List<Path> resultado = new ArrayList<Path>();
+		try (DirectoryStream<Path> stream = Files.newDirectoryStream(diretorio, "*.{dat}")) {
+			for (Path path : stream) {
+				resultado.add(path);
+			}
+		} catch (DirectoryIteratorException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return resultado;
+	}
+	
+	static FileFilter filter = new FileFilter() {
+		@Override
+		public boolean accept(File pathname) {
+			int tamFileName = pathname.getName().length();
+			return pathname.getName().substring(tamFileName - 4, tamFileName).equals(".dat") ? true : false;
+		}
+	};
 }
